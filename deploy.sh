1a91ba91e8bffdae72e47b58c290be0f84de4d39
DHAMAYANDHI R <dhamayandhir.22cse@kongu.edu>
2025-03-20 11:38:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u Dhamaya2004 -p dckr_pat_-YClUH8lgEpTNN-4tTgigUnjZfE
-    docker tag test Dhamaya2004/task2
+    docker tag test1 Dhamaya2004/task2
     docker push Dhamaya2004/task2
     
